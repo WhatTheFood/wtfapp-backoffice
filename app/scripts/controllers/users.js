430dc6ab4d29eaf10ccfbf7faadd90c340c737a0
loic lefloch <l.lefloch.91@gmail.com>
2015-09-27 01:19:41 +0100

    Add admin possibility to change user role

--- app/scripts/controllers/users.js
+++ app/scripts/controllers/users.js
@@ -2,37 +2,49 @@
 
 
 app.controller('UsersCtrl', function ($scope,
-                                         ApiService) {
+                                      ApiService) {
 
   $scope.orderBy = 'email';      // set the default sort type
   $scope.sortReverse = false;   // set the default sort order
-  $scope.search   = '';     // set the default search/filter term
+  $scope.search = '';     // set the default search/filter term
 
   ModalLoading.show();
-    ApiService.getUsers().then(
-      function (response) {
-        console.log('OK', response);
-        $scope.users = response.data;
-        ModalLoading.hide();
-      },
-      function (response) {
-        // TODO: handle error response
-        console.log('KO', response);
-        ModalLoading.hide();
-      }
+  ApiService.getUsers().then(
+    function (response) {
+      console.log('OK', response);
+      $scope.users = response.data;
+      ModalLoading.hide();
+    },
+    function (response) {
+      // TODO: handle error response
+      console.log('KO', response);
+      ModalLoading.hide();
+    }
+  );
 
-    );
+  $scope.changeRole = function(user, role) {
+    ModalLoading.show();
+    ApiService.postRole(user, role).then(function(response) {
+      ModalLoading.hide();
+      console.log('OK', response);
+      user.role = role;
 
-  });
+    }, function(response) {
+      ModalLoading.hide();
+      // TODO
+    })
+  }
+
+});
 
 
 var ModalLoading = {
-  show : function () {
+  show: function () {
     angular.element('#loading').fadeIn();
     angular.element('#loading-body').fadeIn();
   },
 
-  hide : function() {
+  hide: function () {
     angular.element('#loading').fadeOut();
     angular.element('#loading-body').fadeOut();
   }
