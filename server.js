8c7d01eaaf1b5798de4f33f0b87dc0e51f62867b
fel <fxkuntz@gmail.com>
2015-10-07 19:19:17 +0200

    fixes express gzip dependency

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var gzippo = require('gzippo');
+var compression = require('compression');
 var express = require('express');
 var app = express();
 var logger = require('morgan');
 
 app.use(logger);
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+app.use(compression("" + __dirname + "/dist"));
+app.listen(process.env.PORT || 9000);
